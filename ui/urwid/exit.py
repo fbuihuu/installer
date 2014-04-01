90514435b21b40a2c29b8114121290225e780184
Franck Bui <fbui@mandriva.com>
2014-04-01 10:53:00 +0200

    Use ClickableTextPile() in exit view
    action in this view can be internationalized now.

--- ui/urwid/exit.py
+++ ui/urwid/exit.py
@@ -9,21 +9,29 @@ from settings import settings
 
 class ExitView(StepView):
 
-    _actions = ["Quit", "Reboot", "Shutdown"]
-
     def __init__(self, ui, step):
         StepView.__init__(self, ui, step)
 
         self.page = widgets.Page()
         # Make the list centered inside its container
-        body = widgets.ClickableTextList(self._actions, self.on_click)
-        body = urwid.Filler(body, 'middle', height=('relative', 40))
+        body = widgets.ClickableTextPile([(_("Quit"),     self.on_quit),
+                                          (_("Reboot"),   self.on_reboot),
+                                          (_("Shutdown"), self.on_shutdown)])
+        body = urwid.Filler(body, 'middle')
         body = urwid.Padding(body, align='center', width=('relative', 60))
         self.page.body = body
 
     def redraw(self):
         self.page.title = _("Your system is ready to be used")
 
-    def on_click(self, entry):
-        settings.exit.action = entry.text
+    def on_quit(self, widget):
+        settings.exit.action = "Quit"
+        self.run()
+
+    def on_reboot(self, widget):
+        settings.exit.action = "Reboot"
+        self.run()
+
+    def on_shutdown(self, widget):
+        settings.exit.action = "Shutdown"
         self.run()
