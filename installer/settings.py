fbuihuu/installer

commit 3b1cf714f87e47cf03092609638ffea082c6dea6

settings: values from conf file are now typed based on the type of their default values

installer/settings.py
@@ -60,12 +60,12 @@ class Kernel(Section):
 class Options(Section):
     logfile  = '/tmp/installer.log'
     hostonly = True
-    _firmware = None
+    _firmware = []
 
     @property
     def firmware(self):
         if self._firmware:
-            return self._firmware.split()
+            return self._firmware
         return ['uefi' if is_efi() else 'bios']
 
     @firmware.setter
@@ -143,7 +143,23 @@ def load_config_file(config_file):
     config.read(configuration_file)
 
     for section in config.sections():
-        for entry, value in config.items(section):
+        for entry in config.options(section):
+
+            default = settings.get(section, entry)
+            if type(default) == int:
+                getter = config.getint
+            elif type(default) == bool:
+                getter = config.getboolean
+            elif type(default) == float:
+                getter = config.getfloat
+            else:
+                getter = config.get
+
+            value = getter(section, entry)
+
+            if type(default) == list:
+                value = value.split()
+
             settings.set(section, entry, value)
 
 

bin/installer
@@ -88,7 +88,7 @@ def main():
     # Set firmware.
     #
     if args.firmware:
-        settings.Options.firmware = " ".join(args.firmware)
+        settings.Options.firmware = args.firmware
 
     #
     # Start the frontend interface.

commit d5691455426bec5d269949617b2099ee7a062254

settings: fix packages.extra handling broken by 3b1cf714f87e

installer/settings.py
@@ -81,15 +81,16 @@ class Packages(Section):
         return self._extras
 
     @extras.setter
-    def extras(self, pkgfile):
+    def extras(self, pkgfiles):
         #
         # Relative path is relative to the directory
         # containing the config file.
         #
-        pkgfile = os.path.join(os.path.dirname(configuration_file), pkgfile)
-        if not os.path.exists(pkgfile):
-            raise SettingsError("Can't find package list file %s" % pkgfile)
-        self._extra.append(pkgfile)
+        for f in pkgfiles:
+            f = os.path.join(os.path.dirname(configuration_file), f)
+            if not os.path.exists(f):
+                raise SettingsError("Can't find package list file %s" % f)
+            self._extras.append(f)
 
 
 class Steps(Section):