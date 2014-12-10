da87e5ac4c9d4b8c3db58b0d909d83238ac0574b
Franck Bui <fbui@mandriva.com>
2014-12-10 16:57:18 +0100

    local-media: add a workaround for mandriva
    unfortunately urpm(1) only caches *downloaded* packages, ie not *installed* one.

--- installer/steps/local_media.py
+++ installer/steps/local_media.py
@@ -58,24 +58,14 @@ target system can't be found.""")
         if not self._skip:
             # Inform urpmi to keep all installed packages
             distro.urpmi_add_extra_options(['--noclean'])
+            #
             # Do sanity checkings early on the packages file list but
-            # don't store the result to allow the user to do late
-            # modification without the need to restart the installer.
+            # don't store the result to allow the user to do
+            # modifications while the installer is running.
+            #
             settings.LocalMedia.packages
 
     def _process(self):
-        #
-        # For some reasons, urpmi won't populate /var/cache/urpmi/rpms
-        # dir of the target if the repo used during the installation
-        # is local (even though we used --no-clean and --urpmi-root
-        # options. This behaviour breaks this step, so inform the user:
-        #
-        # Unfortunately this won't trap the case where local *and*
-        # remote repositories are used together.
-        #
-        if not os.listdir(self._root + '/var/cache/urpmi/rpms'):
-            raise StepError(self._BROKEN_URPMI_MSG)
-
         #
         # Since 'location' has no default value, it will raise a
         # SettingsError exception if the user did not set it up
@@ -83,12 +73,54 @@ target system can't be found.""")
         #
         dst = settings.LocalMedia.location
 
-        if settings.LocalMedia.packages:
-            self._urpmi(settings.LocalMedia.packages, 50, ['--no-install'])
+        #
+        # There's no way to request urpmi(1) to keep *installed*
+        # packages somewhere for later reinstallation.
+        #
+        # It only keeps *downloaded* packages (in the target at
+        # /var/cache/urpmi/rpms) so if the repo used during the
+        # installation is local, none of the installed packages will
+        # be saved (even though we used both --no-clean and
+        # --urpmi-root options).
+        #
+        # This behaviour makes impossible to implement correctly this
+        # step.
+        #
+        # As a workaround, we do the following:
+        #
+        #   - if the local media is already present on the host
+        #     system, import *all* packages from it. We assume that
+        #     the host was installed by us with the same package list.
+        #
+        #   - if only local medias were used during the installation
+        #     step then the cache is empty (it's been cleaned up
+        #     before starting). Trap this case and fail.
+        #
+        #   - otherwise either a mix of local/remote medias or only
+        #     remotes medias were used. We currently can't detect the
+        #     case (unless we parse urpmi.cfg) so we hope for the
+        #     better :-/
+        #
+        if os.path.exists(dst):
+            self.logger.info(_('Importing host\'s media %s') % dst)
+            self._chroot(['mkdir', '-p', dst])
+            self._rsync(dst + '/', self._root + dst, 50)
+            self._chroot(['sh', '-c', 'rm -f /var/cache/urpmi/rpms/*.rpm'])
 
-        self._chroot(['mkdir', '-p', dst])
-        self._rsync('/var/cache/urpmi/rpms/', dst, 60, rootfs=self._root,
-                    options=['--remove-source-files'])
+        elif not os.listdir(self._root + '/var/cache/urpmi/rpms'):
+            # Unfortunately this won't trap the case where local *and*
+            # remote repositories are used together.
+            raise StepError(self._BROKEN_URPMI_MSG)
+
+        else:
+            self.logger.info(_('Populating media %s using urpmi(1) cache') % dst)
+            # assume that only remote media will be used.
+            if settings.LocalMedia.packages:
+                self._urpmi(settings.LocalMedia.packages, 50, ['--no-install'])
+
+            self._chroot(['mkdir', '-p', dst])
+            self._rsync('/var/cache/urpmi/rpms/', dst, 60, rootfs=self._root,
+                        options=['--remove-source-files'])
 
         self._urpmi(['genhdlist2'], 70)
         self._chroot(['genhdlist2', '--xml-info', '--clean', dst])
